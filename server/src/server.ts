452c22d6e7c80953d3e0aeb989a5a6767e437812
Reinaldo J. Nunes <reinaldo.nunes2@gmail.com>
2023-01-19 16:35:51 -0300

    Refactor & Feat: reorganized the pasta structure and defined the native routes of the backend.

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,9 @@
 //
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient }  from '@prisma/client'
+import { appRoutes } from './routes'
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
 // delimitando acessos
@@ -13,28 +12,12 @@ app.register(cors)
 // origin: ['http://localhost:3000']
 // })
 
-app.get('/', async() => {
-  const habits = await prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: 'Beber'
-      }
-    }
-  })
-
-  return habits
-})
-
-app.get('/all-habits', async() => {
-  const habits = await prisma.habit.findMany()
-
-  return habits
-})
+app.register(appRoutes)
 
 app.listen({
-  port: 9090,
+  port: 5555,
 }).then(() => {
-  console.log("HTTP Server running....")
+  console.log("HTTP Server running.")
 })
 
 
