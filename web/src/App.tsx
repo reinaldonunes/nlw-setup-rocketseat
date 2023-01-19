734dbb8adc63a7721593ac25b211b0855e98fa21
Reinaldo J. Nunes <reinaldo.nunes2@gmail.com>
2023-01-19 11:54:15 -0300

    Feat: defined setup to front-end

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import './styles/global.css'
+
+
+import Habit from './components/Habit'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Habit completed={3} />
+    </div>
+  )
+}
+
+export default App
