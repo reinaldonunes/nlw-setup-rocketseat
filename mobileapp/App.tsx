cd7999a28082640f94988a878fac2c617436662c
Reinaldo J. Nunes <reinaldo.nunes2@gmail.com>
2023-01-20 17:24:28 -0300

    Feat: defined and built mobile interface components and inclusion of dayjs

--- mobileapp/App.tsx
+++ mobileapp/App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { Loading } from './src/components/Loading';
 import { 
   useFonts, 
@@ -8,6 +8,7 @@ import {
   Inter_800ExtraBold,
   Inter_900Black
 } from '@expo-google-fonts/inter'
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,22 +26,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello Android</Text>
+    <>
+      <Home />
       <StatusBar barStyle="light-content" backgroundColor='transparent' translucent />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
